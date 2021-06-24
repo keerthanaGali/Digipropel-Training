12b0544f2b0c1b768c8c3fac954196d40e1ac5b4
keerthanaGali <86400439+keerthanaGali@users.noreply.github.com>
2021-06-24 17:09:04 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace C_programs
+{
+    class Program
+    {
+        int i=10;
+        static string a = "keerthi";
+        static void Main(string[] args)
+        {
+           objectoriented display = new objectoriented();
+           display.Display();
+        }
+
+    }
+}
---
+++ objectoriented.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace C_programs
+{
+    class objectoriented
+    {
+        int i=10;
+        static string a = "keerthi";
+        public  void Display()
+        {
+            
+          // Console.WriteLine(i);
+            Method();
+        }
+        public  void Method()
+        {
+            Console.WriteLine("DPEI0152");
+            Method1();
+        }
+
+        public void Method1()
+        {
+            Console.WriteLine(a);
+        }
+        
+    }
+}
