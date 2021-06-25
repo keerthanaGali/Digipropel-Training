9bb9675f23b3764babd2e71224ac265e93ae0b4d
keerthanaGali <86400439+keerthanaGali@users.noreply.github.com>
2021-06-25 12:47:52 +0530

    Add files via upload

---
+++ june24.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace assign2
+{
+    class june24
+    {
+        //static int i;
+        //static string a = "keerthi";
+        static void Main(string[] args)
+        {
+            //static int i;
+            //static string a = "keerthi";
+            string g;
+            g = Console.ReadLine();
+            Console.WriteLine(g);
+
+
+        }
+
+    }
+}
