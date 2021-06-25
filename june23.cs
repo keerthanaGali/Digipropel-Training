33e860f0870537b81623c9284d5ea320f135a249
keerthanaGali <86400439+keerthanaGali@users.noreply.github.com>
2021-06-25 12:48:49 +0530

    Add files via upload

---
+++ june23.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace C_programs
+{
+    class Program
+    {
+        static int i;
+        static string a = "keerthi";
+        static void Main(string[] args)
+        {
+            objectoriented display = new objectoriented();
+            display.Display();
+        }
+
+    }
+}
