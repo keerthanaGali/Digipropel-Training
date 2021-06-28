cd4506501cf2d0d16acaa4f735e8984cf538aad8
keerthanaGali <86400439+keerthanaGali@users.noreply.github.com>
2021-06-28 22:47:32 +0530

    Add files via upload

--- june28-1.cs
+++ june28-1.cs
@@ -15,17 +15,17 @@ namespace june
    
             Console.Write("Input {0} elements in the array :\n",n);
 
-        for(i=0;i<n;i++)
-        {
-	        Console.Write("element - {0} : ",i);
-		    a[i] = Convert.ToInt32(Console.ReadLine()); 
-	    }
+            for(i=0;i<n;i++)
+            {
+                Console.Write("element - {0} : ",i);
+                a[i] = Convert.ToInt32(Console.ReadLine()); 
+            }
 
-        for(i=0; i<n; i++)
-        {
-            
-            sum += a[i];
-        }
+            for(i=0; i<n; i++)
+            {
+                
+                sum += a[i];
+            }
 
             Console.Write("Sum of all elements stored in the array is :" + sum);
          
