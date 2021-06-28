f6d2051fab8db2618d5fe2dae90beded4156572a
keerthanaGali <86400439+keerthanaGali@users.noreply.github.com>
2021-06-28 22:51:14 +0530

    Add files via upload

---
+++ june28-2.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace june
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[5];
+            int i, j, n, count = 0;
+ 
+            
+            Console.Write("Input the number of elements to be stored in the array : ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input {0} elements in the array :\n",n);
+
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("element - {0} : ",i);
+                a[i] = Convert.ToInt32(Console.ReadLine());           
+            }
+ 
+            for (i = 0; i < n; i++)
+            {
+            for (j = i + 1; j < n; j++)
+            {
+                if (a[i] == a[j])
+                {
+                    count++;
+                    break;
+                }
+            }
+        }
+ 
+            Console.Write("Total number of duplicate elements stored in the array : {0} \n\n ", count);
+    
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
