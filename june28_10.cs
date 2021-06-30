43a09f31f841b8ac036a281be0240ba172e096f5
keerthanaGali <86400439+keerthanaGali@users.noreply.github.com>
2021-06-30 16:24:16 +0530

    Add files via upload

---
+++ june28_10.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace June
+{
+    class june28_10
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[5];
+            int[] b = new int[5];
+            int i, j, s1 = 0, s2 = 0, sum;
+
+            Console.WriteLine("\nEnter the first array elements are : ");
+            for (i = 0; i < a.Length; i++)
+            {
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("\nEnter the second array elements to are : ");
+            for (j = 0; j < b.Length; j++)
+            {
+                b[j] = Convert.ToInt32(Console.ReadLine());
+            }
+            
+            for (i = 0; i < a.Length; i++)
+            {
+                s1 += a[i];
+            }
+
+            for (j = 0; j < b.Length; j++)
+            {
+                s2 += b[j];
+            }
+            sum = s1 + s2;
+            
+
+            Console.WriteLine("sum of the two array elements are : " + sum);
+            Console.Write("\n");
+        }
+    }
+}
