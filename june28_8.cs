c8c3bbd30df2ae22f16d1feaed2a87b76dd3e4b8
keerthanaGali <86400439+keerthanaGali@users.noreply.github.com>
2021-07-01 23:33:43 +0530

    Add files via upload

---
+++ june28_8.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace June
+{
+    class june28_8
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[50];
+            int i,n;
+ 
+            
+            Console.Write("Enter the number of elements to be stored in the array : ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter {0} numbers of elements in the array :\n",n);
+
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("element - {0} : ",i);
+                a[i] = Convert.ToInt32(Console.ReadLine());           
+            }
+ 
+            Console.Write("The values store into the array are : \n");
+            for(i=0;i<n;i++)
+            {
+	            Console.Write("{0}  ",a[i]);
+            }
+        
+            Console.Write("\nThe values store into the array in reverse are :\n");
+            for(i = n-1; i >= 0; i--)
+            {
+                Console.Write("{0}  ",a[i]);
+            }
+
+        }
+    }
+}
+
