784aae477cb48865e894050b62e99f6dcce118b8
keerthanaGali <86400439+keerthanaGali@users.noreply.github.com>
2021-07-02 08:30:56 +0530

    Add files via upload

---
+++ june28_6.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace June
+{
+    class june28_6
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            int i;
+            
+            Console.WriteLine("Enter the array elements : ");
+            for (i = 0; i < arr.Length; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int diff = arr[0] - 0;
+            Console.WriteLine("Missing number is :  ");
+            for (i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] - i != diff)
+                {
+                    while (diff < arr[i] - i)
+                    {
+                        Console.Write(i + diff + " ");
+                        diff++;
+                    }
+                }
+            }        
+        }
+    }
+    
+}
