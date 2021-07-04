9e6193083dbd9caa4f42dd604d5a7638fcdda9a6
keerthanaGali <86400439+keerthanaGali@users.noreply.github.com>
2021-07-04 14:00:47 +0530

    Add files via upload

---
+++ june30_4.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace july
+{
+    class june30_4
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            Console.WriteLine("Enter the string value :");
+            str = Console.ReadLine();
+            int strLength = str.Length;
+            Console.WriteLine("Total number of words in the string is : " + strLength);
+
+        }
+
+    }
+    
+}
