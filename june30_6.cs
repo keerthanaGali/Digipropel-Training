46f12a1a3ad739701d02c201af8fe9efe39bbfe9
keerthanaGali <86400439+keerthanaGali@users.noreply.github.com>
2021-07-04 14:30:52 +0530

    Add files via upload

---
+++ june30_6.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace july
+{
+    class june30_6
+    {
+        static void Main(string[] args)
+        {
+            string str = "The quick brown fox jumps over the lazy dog.";
+            Console.WriteLine("\nOriginal string is : {0}",  str);
+            Console.WriteLine("\nPosition of the word 'fox' in the said string : " + WordPosition(str, "fox"));
+            Console.WriteLine("\nPosition of the word 'the' in the said string : " + WordPosition(str, "the"));
+            Console.WriteLine("\nPosition of the word 'lazy' in the said string : " + WordPosition(str, "lazy"));
+            Console.WriteLine("\n");
+        }
+        public static int WordPosition(string text, string word)
+        {
+            string[] arry = text.Split(' '); 
+            return Array.IndexOf(arry, word) + 1;
+        }
+
+        
+
+    }
+    
+}
