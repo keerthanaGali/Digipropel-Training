941ce5af07fb60f00147a5943993f196711ddb70
keerthanaGali <86400439+keerthanaGali@users.noreply.github.com>
2021-07-07 16:31:03 +0530

    Update june28_4.cs

--- june28_4.cs
+++ june28_4.cs
@@ -49,3 +49,22 @@ namespace June
 
     }
 }
+
+/**
+Input & Output :
+--------------------
+Enter the number of elements to be stored in the array : 7
+Enter 7 elements in the array :
+element - 0 : 2
+element - 1 : 5
+element - 2 : 1
+element - 3 : 8
+element - 4 : 3
+element - 5 : 6
+element - 6 : 1
+
+The Even elements are :
+2 8 6
+The Odd elements are :
+5 1 3 1
+**/
