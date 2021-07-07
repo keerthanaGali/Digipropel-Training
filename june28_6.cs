cce4240e7243bc58b90cd215f133d6d7b8cee32e
keerthanaGali <86400439+keerthanaGali@users.noreply.github.com>
2021-07-07 17:11:11 +0530

    Update june28_6.cs

--- june28_6.cs
+++ june28_6.cs
@@ -15,7 +15,7 @@ namespace June
                 arr[i] = Convert.ToInt32(Console.ReadLine());
             }
             int diff = arr[0] - 0;
-            Console.WriteLine("Missing number is :  ");
+            Console.WriteLine("Missing numbers is :  ");
             for (i = 0; i < arr.Length; i++)
             {
                 if (arr[i] - i != diff)
@@ -31,3 +31,37 @@ namespace June
     }
     
 }
+
+/**
+Input & Output : (one example)
+-------------------------------
+Enter the array elements : 
+10
+11
+13
+14
+15
+16
+17
+18
+19
+20
+Missing number is :  
+12
+
+Input & Output : (another example)
+-------------------------------------
+Enter the array elements : 
+3
+5
+8
+10
+13
+16
+18
+19
+20
+21
+Missing numbers is :
+4 6 7 9 11 12 14 15 17
+**/
