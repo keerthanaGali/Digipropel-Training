cbd51d17153a8b43f5abbe7c039849ef1f5be9c6
keerthanaGali <86400439+keerthanaGali@users.noreply.github.com>
2021-07-08 19:59:37 +0530

    Add files via upload

---
+++ july05_2.cs
@@ -0,0 +1,61 @@
+﻿/**2. Create a C# program that prompts the user for three names of people and stores them in an array of Person-type objects.**/
+
+using System;
+
+namespace july
+{
+    class july05_2 
+    {
+        //Protected class instead of private
+        static void Main(string[] args)
+        {
+            int a = 3;
+            string Name;
+            Person[] persons = new Person[a];
+            for (int i = 0; i < a; i++)
+            {
+                persons[i] = new Person(Name = Console.ReadLine());
+            }
+            for (int i = 0; i < a; i++)
+            {
+                Console.WriteLine(persons[i]);
+            }
+        }
+    }
+    
+    public class Person
+    {
+        public string Name { get; set;}
+        public Person(string name)
+        {
+            Name = name;
+        }
+        public override string ToString()
+        {
+           Console.WriteLine("Constructors are called : ");
+            return "Name :" + Name;
+        }
+         ~Person()
+         {
+             Name = string.Empty;
+         }
+    }
+    
+}
+
+/**
+Input : 
+-------------
+keerthi
+vineeth
+prathyusha
+
+Output :
+--------------
+Constructors are called : 
+Name :keerthi
+Constructors are called :
+Name :vineeth
+Constructors are called :
+Name :prathyusha   
+**/
