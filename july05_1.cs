ca7b4256fb87cb78f02bed9bb9b49a567a5fc609
keerthanaGali <86400439+keerthanaGali@users.noreply.github.com>
2021-07-08 10:46:56 +0530

    Add files via upload

---
+++ july05_1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace july
+{
+    
+    class july05_1
+    {
+        static void Main(string[] args)
+        {
+            int i, total = 3;
+            Person[] persons = new Person[total];
+    
+            for (i = 0; i < total; i++)
+            {
+                persons[i] = new Person() 
+                {
+                    Name = Console.ReadLine()
+                };
+            }
+    
+            for (i = 0; i < total; i++)
+            {
+                Console.WriteLine(persons[i].ToString());
+            }
+    
+            Console.ReadLine();
+        }
+    
+        public class Person
+        {
+            public string Name { get; set; }
+    
+            public override string ToString()
+            {
+                return "Hello! My name is " + Name;
+            }
+        }
+    }
+}
