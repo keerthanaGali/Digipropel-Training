decad6b870b6117ed96bf1569aa370ecd0b5d504
keerthanaGali <86400439+keerthanaGali@users.noreply.github.com>
2021-07-08 20:43:00 +0530

    Add files via upload

---
+++ july05_6.cs
@@ -0,0 +1,41 @@
+﻿/**6. Write a C# program to check if the entered year is a leap year or not**/
+
+using System;
+
+namespace july
+{
+    class july05_6
+    {
+        static void Main(string[] args)
+        {
+            // Check if the year is leap year or not
+            int Year;
+            Console.Write("\nEnter the Year : ");
+            Year = Convert.ToInt32(Console.ReadLine());
+
+            // If it is disvible by 100 or 400 then it is a Leap Year
+            if((Year % 100 == 0) || (Year % 400 == 0) || (Year % 4 == 0))
+            {
+                Console.WriteLine("It is a Leap Year");
+            }
+            //If it is not disvible by 100 or 400 then it is not a Leap Year
+            else
+            {
+                Console.WriteLine("It is not a Leap Year");
+            }
+            Console.Write("\n");
+        }
+    }
+}
+
+/**
+Input & Output : 
+------------------
+Enter the Year : 2020
+It is a Leap Year
+
+Another Input & Output :
+--------------------------
+Enter the Year : 1998
+It is not a Leap Year
+**/
