e835318eb2365f9c373783cf86cdc29d44da6ea5
keerthanaGali <86400439+keerthanaGali@users.noreply.github.com>
2021-07-10 21:47:16 +0530

    Update june30_8.cs

--- june30_8.cs
+++ june30_8.cs
@@ -1,4 +1,6 @@
-﻿using System;
+/**8. Write a C# Sharp program to alternate the case of each letter in a given string and the first letter of the said string must be uppercase.**/
+
+using System;
 
 namespace july
 {
@@ -7,7 +9,7 @@ namespace july
         static void Main(string[] args)
         {
             string str = "c# Exercises";
-            Console.WriteLine("Original string:\n" + str);
+            Console.WriteLine("\nOriginal string:\n" + str);
             Console.WriteLine("\nAfter alternating the case of each letter of the given string:\n" + test(str));
 
             str = "C# is used to develop web apps, desktop apps, mobile apps, games and much more.";
@@ -36,3 +38,19 @@ namespace july
     }
     
 }
+
+/**
+Output :
+--------------
+Original string:
+c# Exercises
+
+After alternating the case of each letter of the given string:
+C# ExErCiSeS
+
+Original string:
+C# is used to develop web apps, desktop apps, mobile apps, games and much more.
+
+After alternating the case of each letter of the given string:
+C# Is UsEd To DeVeLoP wEb ApPs, dEsKtOp ApPs, mObIlE aPpS, GaMeS aNd MuCh MoRe.
+**/
