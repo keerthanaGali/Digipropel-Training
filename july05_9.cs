c41e1dd64b0b5d7c41f26f25906c9a932277d194
keerthanaGali <86400439+keerthanaGali@users.noreply.github.com>
2021-07-11 21:01:58 +0530

    Add files via upload

---
+++ july05_9.cs
@@ -0,0 +1,45 @@
+﻿/**9. Make D:\csharp\example.dat file using BinaryWriter class, store current date and time and read that text using BinaryReader class.**/
+
+using System;
+using System.IO;
+
+namespace july
+{
+    class july05_9
+    {
+        // Define constant string
+        const string filename = "july.date and time"; 
+        static void Main(string[] args)
+        {
+            WriteDefaultValues();
+            DisplayValues();
+        }
+        
+        public static void WriteDefaultValues()
+        {
+            using (BinaryWriter writer = new BinaryWriter(File.Open(filename, FileMode.Create)))
+            {
+                writer.Write("Current Date and Time : " + DateTime.Now.ToString());
+            }
+        }
+        public static void DisplayValues()
+        {
+            if (File.Exists(filename))
+            {
+                using (BinaryReader reader = new BinaryReader(File.Open(filename, FileMode.Open)))
+                {
+                    Console.WriteLine(reader.ReadString());
+                }
+            }
+        }
+
+    }
+}
+
+
+
+/**
+Output : 
+------------------
+Current Date and Time : 11-07-2021 21:01:16
+**/
