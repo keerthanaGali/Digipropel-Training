3b1bceb74f1c8280f631118dbc7528b339c3db6e
keerthanaGali <86400439+keerthanaGali@users.noreply.github.com>
2021-07-11 20:28:17 +0530

    Add files via upload

---
+++ july05_8.cs
@@ -0,0 +1,26 @@
+﻿/**8. Write a c# program to generate a random number.**/
+
+using System;
+
+namespace july
+{
+    class july05_8
+    {
+        static void Main(string[] args)
+        {
+            Random random = new System.Random();
+            //will generate randon numbers
+            Console.WriteLine("First random number is :" +(random.Next(0,10)));
+            Console.WriteLine("Second random number is : "+(random.Next(20,100)));
+        }
+    }
+}
+
+
+
+/**
+Output : 
+------------------
+First random number is :6
+Second random number is : 91
+**/
